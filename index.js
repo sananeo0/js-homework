228a0e6abdcd255ac3ce8c161fae3a2a4378a67c
sananeo0 <sananeo@code.edu.az>
2023-10-22 17:52:43 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+//task 1 
+
+// var student = {
+//     name: "David Rayy",
+//     sclass: "VI",
+//     rollno: 12
+// };
+
+// var properties = Object.keys(student);
+// console.log(properties);
+
+
+//task 2 
+
+// var student = {
+//     name: "David Rayy",
+//     sclass: "VI",
+//     rollno: 12
+// };
+
+// console.log("Object before deletion:");
+// console.log(student);
+
+// delete student.rollno;
+// console.log("Object after deleting 'rollno' property:");
+// console.log(student);
+
+
+
+//exercise 3 
+
+// const objWithSum = {
+//     a: 10,
+//     b: 10
+// };
+
+// const updatedObj = { ...objWithSum, sum: objWithSum.a + objWithSum.b };
+
+// console.log(updatedObj);
+
+
+//exercise 4 
+
+// const objWithRes = {
+//     n: 13,
+//     info: "n is smaller than 20"
+// };
+
+// if (objWithRes.n > 10) {
+//     const updatedObj = {
+//         ...objWithRes,
+//         info: "n is bigger than 10"
+//     };
+//     console.log(updatedObj);
+// }
+
+// else {
+//     console.log(objWithRes);
+// }
